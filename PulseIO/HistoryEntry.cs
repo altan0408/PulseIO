67c08ef5d0c3e20241f6ac417dacb1696371a5de
altan0408 <andrlssntn@gmail.com>
2026-06-17 20:42:01 +0800

    New updates on Managers

---
+++ PulseIO/HistoryEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PulseIO
+{
+    public class HistoryEntry
+    {
+        public DateTime Timestamp { get; set; }
+
+        public string DeviceName { get; set; }
+
+        public string EventType { get; set; }
+    }
+}
---
+++ PulseIO/Managers/HistoryManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace PulseIO
+{
+    internal class HistoryManager
+    {
+        private readonly List<HistoryEntry> _history =
+            new List<HistoryEntry>();
+
+        public void AddHistory(string deviceName, string eventType)
+        {
+            _history.Add(new HistoryEntry
+            {
+                Timestamp = System.DateTime.Now,
+                DeviceName = deviceName,
+                EventType = eventType
+            });
+        }
+
+        public List<HistoryEntry> GetHistory()
+        {
+            return new List<HistoryEntry>(_history);
+        }
+
+        public void ClearHistory()
+        {
+            _history.Clear();
+        }
+
+        public int Count
+        {
+            get { return _history.Count; }
+        }
+    }
+}
